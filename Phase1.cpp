39b3cbfeb95bc3980bdd07cbf415810efef98b67
IPKCHK <istarlakalyananudeep@gmail.com>
2024-02-23 20:37:19 +0530

    as

--- Phase1.cpp
+++ Phase1.cpp
@@ -2,7 +2,6 @@
 #include<vector>
 #include<fstream>
 #include<sstream>
-#include<string>
 using namespace std;
 class core
 {
@@ -12,40 +11,53 @@ class core
     int pc=1;
     string *program=new string[1000];
     vector<string> parts;
-    string h;
-    void execute()
+
+    void execute(int pc)
     {
-        stringstream string(program[pc]);
-        while(getline(string,h,' '))
+        int base;
+        string h;
+        string f;
+        stringstream str(program[pc]);
+        while(getline(str,h,' '))
         {
             parts.push_back(h);
         }
+        if(parts[0]=="base:")
+        {
+            base=stoi(parts[1]);
+        }
         if(parts[0]=="add")
         {
-                        int rd,rs1,rs2;
+            int rd,rs1,rs2;
             if(parts[1].size()>2)
             {
-                rd=int((parts[1])[1]+parts[1][2]);
+                f=string(1,parts[1][1])+string(1,parts[1][2]);
+                rd=stoi(f);
             }
             if(parts[1].size()==2)
             {
-                rd=int((parts[1])[1]);
+                f=string(1,parts[1][1]);
+                rd=stoi(f);
             }
             if(parts[2].size()>2)
             {
-                rs1=int((parts[2])[1]+parts[2][2]);
+                f=string(1,parts[2][1])+string(1,parts[2][2]);
+                rs1=stoi(f);
             }
             if(parts[2].size()==2)
             {
-                rs1=int((parts[2])[1]);
+                f=string(1,parts[2][1]);
+                rs1=stoi(f);
             }
             if(parts[3].size()>2)
             {
-                rs2=int((parts[3])[1]+parts[3][2]);
+                f=string(1,parts[3][1])+string(1,parts[3][2]);
+                rs2=stoi(f);
             }
             if(parts[3].size()==2)
             {
-                rs2=int((parts[3])[1]);
+                f=string(1,parts[3][1]);
+                rs2=stoi(f);
             }
             registers[rd]=registers[rs1]+registers[rs2];
             pc=pc+1;
@@ -53,29 +65,36 @@ class core
         if(parts[0]=="sub")
         {
             int rd,rs1,rs2;
+            int rd,rs1,rs2;
             if(parts[1].size()>2)
             {
-                rd=int((parts[1])[1]+parts[1][2]);
+                f=string(1,parts[1][1])+string(1,parts[1][2]);
+                rd=stoi(f);
             }
             if(parts[1].size()==2)
             {
-                rd=int((parts[1])[1]);
+                f=string(1,parts[1][1]);
+                rd=stoi(f);
             }
             if(parts[2].size()>2)
             {
-                rs1=int((parts[2])[1]+parts[2][2]);
+                f=string(1,parts[2][1])+string(1,parts[2][2]);
+                rs1=stoi(f);
             }
             if(parts[2].size()==2)
             {
-                rs1=int((parts[2])[1]);
+                f=string(1,parts[2][1]);
+                rs1=stoi(f);
             }
             if(parts[3].size()>2)
             {
-                rs2=int((parts[3])[1]+parts[3][2]);
+                f=string(1,parts[3][1])+string(1,parts[3][2]);
+                rs2=stoi(f);
             }
             if(parts[3].size()==2)
             {
-                rs2=int((parts[3])[1]);
+                f=string(1,parts[3][1]);
+                rs2=stoi(f);
             }
             registers[rd]=registers[rs1]-registers[rs2];
             pc=pc+1;
@@ -85,19 +104,23 @@ class core
             int r1,r2;
             if(parts[1].size()>2)
             {
-                r1=int((parts[1])[1]+parts[1][2]);
+                f=string(1,parts[1][1])+string(1,parts[1][2]);
+                r1=stoi(f);
             }
             if(parts[1].size()==2)
             {
-                r1=int((parts[1])[1]);
+                f=string(1,parts[1][1]);
+                r1=stoi(f);
             }
             if(parts[2].size()>2)
             {
-                r2=int((parts[2])[1]+parts[2][2]);
+                f=string(1,parts[2][1])+string(1,parts[2][2]);
+                r2=stoi(f);
             }
-            if(parts[2].size()==2)
+            if(parts[3].size()==2)
             {
-                r2=int((parts[2])[1]);
+                f=string(1,parts[2][1]);
+                r2=stoi(f);
             }
             int y=0;
             if(r1!=r2)
@@ -107,6 +130,7 @@ class core
                     y++;
                     if(program[y]==parts[3])
                         pc=y;
+                        return;
                 }
             }
             else
@@ -119,19 +143,23 @@ class core
             int r1,r2;
             if(parts[1].size()>2)
             {
-                r1=int((parts[1])[1]+parts[1][2]);
+                f=string(1,parts[1][1])+string(1,parts[1][2]);
+                r1=stoi(f);
             }
             if(parts[1].size()==2)
             {
-                r1=int((parts[1])[1]);
+                f=string(1,parts[1][1]);
+                r1=stoi(f);
             }
             if(parts[2].size()>2)
             {
-                r2=int((parts[2])[1]+parts[2][2]);
+                f=string(1,parts[2][1])+string(1,parts[2][2]);
+                r2=stoi(f);
             }
             if(parts[2].size()==2)
             {
-                r2=int((parts[2])[1]);
+                f=string(1,parts[2][1]);
+                r2=stoi(f);
             }
             int y=0;
             if(r1==r2)
@@ -141,6 +169,7 @@ class core
                     y++;
                     if(program[y]==parts[3])
                         pc=y;
+                        return;
                 }
             }
             else
@@ -153,19 +182,23 @@ class core
             int r1,r2;
             if(parts[1].size()>2)
             {
-                r1=int((parts[1])[1]+parts[1][2]);
+                f=string(1,parts[1][1])+string(1,parts[1][2]);
+                r1=stoi(f);
             }
             if(parts[1].size()==2)
             {
-                r1=int((parts[1])[1]);
+                f=string(1,parts[1][1]);
+                r1=stoi(f);
             }
             if(parts[2].size()>2)
             {
-                r2=int((parts[2])[1]+parts[2][2]);
+                f=string(1,parts[2][1])+string(1,parts[2][2]);
+                r2=stoi(f);
             }
             if(parts[2].size()==2)
             {
-                r2=int((parts[2])[1]);
+                f=string(1,parts[2][1]);
+                r2=stoi(f);
             }
             int y=0;
             if(r1<r2)
@@ -175,6 +208,7 @@ class core
                     y++;
                     if(program[y]==parts[3])
                         pc=y;
+                        return;
                 }
             }
             else
@@ -187,19 +221,23 @@ class core
             int r1,r2;
             if(parts[1].size()>2)
             {
-                r1=int((parts[1])[1]+parts[1][2]);
+                f=string(1,parts[1][1])+string(1,parts[1][2]);
+                r1=stoi(f);
             }
             if(parts[1].size()==2)
             {
-                r1=int((parts[1])[1]);
+                f=string(1,parts[1][1]);
+                r1=stoi(f);
             }
             if(parts[2].size()>2)
             {
-                r2=int((parts[2])[1]+parts[2][2]);
+                f=string(1,parts[2][1])+string(1,parts[2][2]);
+                r2=stoi(f);
             }
             if(parts[2].size()==2)
             {
-                r2=int((parts[2])[1]);
+                f=string(1,parts[2][1]);
+                r2=stoi(f);
             }
             int y=0;
             if(r1>=r2)
@@ -209,6 +247,7 @@ class core
                     y++;
                     if(program[y]==parts[3])
                         pc=y;
+                        return;
                 }
             }
             else
@@ -218,21 +257,201 @@ class core
         }
         if(parts[0]=="lw")
         {
+            int x,y;
+            string w;
+            int offset;
+            int r1,r2;
             if(parts[1].size()>2)
             {
-                int r1=int((parts[1])[1]+parts[1][2]);
+                f=string(1,parts[1][1])+string(1,parts[1][2]);
+                r1=stoi(f);
             }
             if(parts[1].size()==2)
             {
-                int r1=int((parts[1])[1]);
+                f=string(1,parts[1][1]);
+                r1=stoi(f);
             }
-
+            if(parts[2][2]=='x'||parts[2][3]=='x')
+            {
+                if(parts[2][2]=='x')
+                {
+                    f=string(1,parts[2][0]);
+                    offset=stoi(f);
+                    if(parts[2].size()==5)
+                    {
+                        w=string(1,parts[2][4]);
+                        r2=stoi(f);                       
+                    }
+                    if(parts[2].size()==6)
+                    {
+                        w=string(1,parts[2][4])+string(1,parts[2][5]);
+                        r2=stoi(f);                       
+                    }
+                }
+                if(parts[2][3]=='x')
+                {
+                    f=string(1,parts[2][0])+string(1,parts[2][1]);
+                    offset=stoi(f);
+                    if(parts[2].size()==6)
+                    {
+                        w=string(1,parts[2][4]);
+                        r2=stoi(f);                       
+                    }
+                    if(parts[2].size()==7)
+                    {
+                        w=string(1,parts[2][4])+string(1,parts[2][5]);
+                        r2=stoi(f);                       
+                    }
+                }
+                registers[r1]=m.mem[registers[r2]+offset];
+            }
+            if(parts[2]=="base")
+            {
+                registers[r1]=m.mem[base];
+            } 
+            pc=pc+1;
+        }
+        if(parts[0]=="sw")
+        {
+            int x,y;
+            string w;
+            int offset;
+            int r1,r2;
+            if(parts[1].size()>2)
+            {
+                f=string(1,parts[1][1])+string(1,parts[1][2]);
+                r1=stoi(f);
+            }
+            if(parts[1].size()==2)
+            {
+                f=string(1,parts[1][1]);
+                r1=stoi(f);
+            }
+            if(parts[2][2]=='x'||parts[2][3]=='x')
+            {
+                if(parts[2][2]=='x')
+                {
+                    f=string(1,parts[2][0]);
+                    offset=stoi(f);
+                    if(parts[2].size()==5)
+                    {
+                        w=string(1,parts[2][4]);
+                        r2=stoi(f);                       
+                    }
+                    if(parts[2].size()==6)
+                    {
+                        w=string(1,parts[2][4])+string(1,parts[2][5]);
+                        r2=stoi(f);                       
+                    }
+                }
+                if(parts[2][3]=='x')
+                {
+                    f=string(1,parts[2][0])+string(1,parts[2][1]);
+                    offset=stoi(f);
+                    if(parts[2].size()==6)
+                    {
+                        w=string(1,parts[2][4]);
+                        r2=stoi(f);                       
+                    }
+                    if(parts[2].size()==7)
+                    {
+                        w=string(1,parts[2][4])+string(1,parts[2][5]);
+                        r2=stoi(f);                       
+                    }
+                }   
+            }
+            m.mem[registers[r2]+offset]=registers[r1];
+            pc=pc+1;
+        }
+        if(parts[0]=="la")
+        {
+            int x,y;
+            string w;
+            int offset;
+            int r1,r2;
+            if(parts[1].size()>2)
+            {
+                f=string(1,parts[1][1])+string(1,parts[1][2]);
+                r1=stoi(f);
+            }
+            if(parts[1].size()==2)
+            {
+                f=string(1,parts[1][1]);
+                r1=stoi(f);
+            }
+            if(parts[2][2]=='x'||parts[2][3]=='x')
+            {
+                if(parts[2][2]=='x')
+                {
+                    f=string(1,parts[2][0]);
+                    offset=stoi(f);
+                    if(parts[2].size()==5)
+                    {
+                        w=string(1,parts[2][4]);
+                        r2=stoi(f);                       
+                    }
+                    if(parts[2].size()==6)
+                    {
+                        w=string(1,parts[2][4])+string(1,parts[2][5]);
+                        r2=stoi(f);                       
+                    }
+                }
+                if(parts[2][3]=='x')
+                {
+                    f=string(1,parts[2][0])+string(1,parts[2][1]);
+                    offset=stoi(f);
+                    if(parts[2].size()==6)
+                    {
+                        w=string(1,parts[2][4]);
+                        r2=stoi(f);                       
+                    }
+                    if(parts[2].size()==7)
+                    {
+                        w=string(1,parts[2][4])+string(1,parts[2][5]);
+                        r2=stoi(f);                       
+                    }
+                }
+                registers[r1]=registers[r2]+offset;   
+            }
+            if(parts[2]=="base")
+            {
+                registers[r1]=base;
+            } 
+            pc=pc+1;
+        }
+        if(parts[0]=="addi")
+        {
+            int rd,rs,int k;
+            if(parts[1].size()>2)
+            {
+                f=string(1,parts[1][1])+string(1,parts[1][2]);
+                rd=stoi(f);
+            }
+            if(parts[1].size()==2)
+            {
+                f=string(1,parts[1][1]);
+                rd=stoi(f);
+            }
+            if(parts[2].size()>2)
+            {
+                f=string(1,parts[2][1])+string(1,parts[2][2]);
+                rs=stoi(f);
+            }
+            if(parts[2].size()==2)
+            {
+                f=string(1,parts[2][1]);
+                rs=stoi(f);
+            }
+            k=stoi(parts[3]);
+            registers[rd]=registers[rs]+k;
+            pc=pc+1;
         }
     }
 };
 class memory
 {
-    int *memory=new int[1024];
+    public:
+    int *mem=new int[1024];
 };
 class processor
 {
@@ -254,6 +473,6 @@ int main()
     while(getline(infile,l))
     {
         c1.program[pc]=l;
-        pc=pc+1;
     }
+    infile.close();
 }
